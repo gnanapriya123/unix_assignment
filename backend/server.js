b5cd89d890e850ceba0998db0f6dc7cf59d7c5e4
Gnana Priya <231121@iiitt.ac.in>
2026-03-29 19:54:22 +0530

    Set up database connection and models

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,16 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const path=require("path");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use(express.static(path.join(__dirname)));
+
 // MongoDB connection
-mongoose.connect("mongodb://mongo:27017/attendance", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-});
+mongoose.connect("mongodb://mongo:27017/attendance");
 
 // Schema
 const AttendanceSchema = new mongoose.Schema({
@@ -31,6 +31,25 @@ app.post("/mark", async (req, res) => {
 app.get("/all", async (req, res) => {
     const data = await Attendance.find();
     res.json(data);
+});
+
+app.delete("/delete/:id", async (req, res) => {
+    try {
+        const deleted = await Attendance.findByIdAndDelete(req.params.id);
+
+        if (!deleted) {
+            return res.status(404).send("Record not found");
+        }
+
+        res.send("Deleted successfully");
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("Error deleting");
+    }
+});
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "index.html"));
 });
 
 app.listen(5000, () => console.log("Server running on port 5000"));
